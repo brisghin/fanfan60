2f358fb0f7779e10c5e51b93824d73e2d6ee6bdb
boris gohin <boris.gohin@yahoo.co.uk>
2020-10-07 10:45:11 +0200

    test yt api + clean

--- src/js/app.js
+++ src/js/app.js
@@ -25,14 +25,14 @@ $(document).ready(function() {
   }
 
   // For player: https://developers.google.com/youtube/iframe_api_reference
+  // let tag = document.createElement('script');
+  // tag.src = "https://www.youtube.com/iframe_api";
+  //
+  // let firstScriptTag = document.getElementsByTagName('script')[0];
+  // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
   let player;
   function onYouTubeIframeAPIReady() { // function name important
-    let tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-
-    let firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    
     let player = new YT.Player('player', { height: '360', width: '640' })
 
     const resetIcones = () => {
